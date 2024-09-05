94e6576fd3684d385b03e6076f77c68edc0a9a71
hhlovesyy <1596944152@qq.com>
2024-09-05 17:21:53 +0800

    添加了第二个maya插件并记录关键注释，雨滴附着表面的效果

--- PythonScripts/DuplicateOverSurfaceExp.py
+++ PythonScripts/DuplicateOverSurfaceExp.py
@@ -6,7 +6,7 @@ from maya import cmds
 # cmds.duplicateOverSurface(cmds.ls(sl=True, long=True)[0], rotation=True, instanceLeaf=True)
 
 # Duplicate selected object over surface but keep original rotations.
-cmds.MyDuplicateOverSurface("pTorus1", rotation=True, instanceLeaf=True)
+# cmds.MyDuplicateOverSurface("pTorus1", rotation=True, instanceLeaf=True)
 
 # 停止使用插件
 # cmds.unloadPlugin("duplicateOverSurface")

f92fb3c1dd6576a1c3affb45d0a0138f42831071
hhlovesyy <1596944152@qq.com>
2024-09-12 18:20:25 +0800

    算是完成了切割插件的核心功能

--- PythonScripts/MySeperatePlugin.py
+++ PythonScripts/MySeperatePlugin.py
@@ -26,15 +26,32 @@ class UVToolKit():
         cmds.checkBox('USE_PLANE', label='使用平面', value=False)
         cmds.checkBox('FLAT', label='FLAT', value=False)
         cmds.checkBox('RESULT_ONLY', label='仅显示结果', value=False)
+        cmds.checkBox('DEL', label="删除原始对象", value=False)
 
         cmds.checkBox('LEGACY', label='LEGACY', value=False)
+        cmds.floatSliderGrp(
+            'MERGE',
+            label="MERGE DISTANCE",
+            field=True,
+            width=350,
+            value=0.001,
+            fieldMinValue=0.001,
+            fieldMaxValue=10000,
+            minValue=0.1,
+            maxValue=100,
+            enable=True,
+            pre=4
+        )
+
+        # 创建一个分隔符，参数与原 MEL 代码相同
+        cmds.separator(width=400, height=10, hr=True, style="in")
         cmds.setParent("..")
 
         cmds.separator(h=10, st='in')
         cmds.rowColumnLayout(w=380)
         # 创建 textFieldGrp 控件
         cmds.textFieldGrp(
-            "textFieldGroup",
+            "GRP_CUT",
             label="GROUP NAME",
             text="pl_shattered_GRP",
             annotation="group name for the result",
@@ -216,10 +233,197 @@ class UVToolKit():
 
         return geo[0]
 
+    def pl_geoHole(self):
+        selected = cmds.ls(sl=True, long=True)
+        # 设置多边形选择约束，以便只选择具有特定类型的面（此处为 0x8000）
+        # mode=3:All and Next : all items satisfying constraints are selected.
+        # type=0x8000(edge)
+        # 	0(off) 1(on border) 2(inside).
+        cmds.polySelectConstraint(mode=3, type=0x8000, where=1)
+        # 检查当前选择是否有物体，如果选择的数量大于 0，则 check 为 1，否则为 0
+        check = 1 if len(cmds.ls(sl=True)) > 0 else 0  # 这个可以拿来即用，应该不太用深究，就是判断是否有洞
+        cmds.polySelectConstraint(disable=True)
+        cmds.select(selected)
+        return check
+
+    def pl_shatterCutBy(self):
+        print("pl_shatterCutBy!!!")
+        rand = random.randint(0, len(cmds.ls(selection=True)) * 1000)
+        # 进这个函数的时候，只选择了两个物体，一个是要被切的，一个是切的物体，所以这里的cmds.ls(selection=True)只有两个物体，tail就是切东西的物体
+        print("List last selected object", cmds.ls(selection=True, long=True, tail=1))  # ['|pl_chipOff_GRP|pCube2_pl_chipOff|pCube2_pl_sphere1']
+        print("cmds.ls(selection=True, long=True, transforms=True)", cmds.ls(selection=True, long=True, transforms=True))  # ['|pCube2', '|pl_chipOff_GRP|pCube2_pl_chipOff|pCube2_pl_sphere1']
+        first = list(set(cmds.ls(selection=True, long=True, transforms=True)) - set(cmds.ls(selection=True, long=True, tail=1)))
+        print("first!!!", first)  # ['|pCube2']  # 搞这么麻烦，这个就是被切的物体，被切完之后会返回被切完的物体，进入for循环被下一次切
+        # 获取被选中的变换节点
+        firstTr = cmds.ls(first, sn=True, transforms=True) # sn:shortName,
+        firstTr[0] = re.sub(r".*[|]", "", firstTr[0])
+        print("firstTr!!!", firstTr)  # ['pCube2']
+        second = cmds.ls(sl=True, l=True, tl=True)[0]
+        print("second!!!", second)  # second!!! |pl_chipOff_GRP|pCube2_pl_chipOff|pCube2_pl_sphere1
+
+        firstCount = len(first)
+        group = []
+        merge = cmds.floatSliderGrp('MERGE', query=True, value=True)
+        legacy = cmds.checkBoxGrp('LEGACY', query=True, value1=True)
+        grpName = cmds.textFieldGrp('GRP_CUT', query=True, text=True)
+        selected_objects = cmds.ls(sl=True, l=True, tr=True)
+
+        if len(selected_objects) < 2:
+            cmds.confirmDialog(
+                title="Select",
+                message="Please select at least two objects!",
+                button=["OK"],
+                defaultButton="OK",
+                ma="center"
+            )
+        else:
+            cmds.select(first, replace=True)
+            if not cmds.objExists("Interior_pl_shatterCutBy"):
+                shader = cmds.shadingNode('lambert', asShader=True, name='Interior_pl_shatterCutBy')
+                cmds.setAttr("Interior_pl_shatterCutBy.color", 0.382, 0.256526, 0.092444)
+            first_group = cmds.listRelatives(p=True, f=True, type='transform')
+            check_grp = None
+            grp = []
+            if first_group:
+                check_grp = any("*pl_shattered*" in name for name in first_group)
+            if not check_grp:
+                cmds.hide(cmds.ls(sl=True))
+                cmds.select(first, replace=True)
+                cmds.duplicate()
+                cmds.showHidden(cmds.ls(sl=True))  # 把原来的cube2隐藏掉，复制出cube3，然后显示cube3
+            else:
+                # 如果包含 "pl_shattered"，则列出第一个对象的父级变换节点
+                grp = cmds.listRelatives(first, p=True, f=True, type='transform', r=True)
+            # 创建组并解组
+            cmds.group()
+            cmds.ungroup()
+            first = cmds.ls(sl=True, l=True, tr=True)
+            print("now first is: ", first)  # now first is:  ['|pCube3']
+            # 删除历史记录
+            cmds.delete(ch=True)
+            # 列出所有父级对象
+            crap_exists_first = cmds.listRelatives(fullPath=True)
+            print("crap_exists_first!!!", crap_exists_first)  # crap_exists_first!!! ['|pCube3|pCube3Shape']
+            if len(crap_exists_first) > 1:
+                for item in crap_exists_first:
+                    if cmds.nodeType(item) != "mesh": # 如果有额外的东西，把除了mesh以外的东西删掉
+                        cmds.delete(item)
+            for s in range(firstCount):
+                cmds.select(first[s], r=True)
+                cmds.polyCloseBorder(ch=False)  # Closes open borders of objects. For each border edge given, a face is created to fill the hole the edge lies on. ch就是历史记录，False就是关闭历史记录
+                cmds.ConvertSelectionToVertices()
+                # 以下这行似乎可以用来处理非流形的情况
+                cmds.polyMergeVertex(d=merge, alwaysMergeTwoVertices=True, ch=False)  # https://download.autodesk.com/us/maya/2009help/commandspython/polyMergeVertex.html
+                cmds.select(first[s], r=True)
+                cmds.polyNormalPerVertex(ufn=True) # ufn=True 参数表示“使用面法线”
+                cmds.makeIdentity(apply=True, t=True, r=True, s=True, normal=0)  # normal=0 可能是默认的，应该不用管了
+                cmds.delete(ch=True)
+            cmds.select(second, r=True)  # 切目标物体的物体
+            geo_hole = self.pl_geoHole()
+            second = cmds.duplicate()
+            cmds.hyperShade(a='Interior_pl_shatterCutBy') # a:assign
+            cmds.delete(ch=True)
+            crap_exists_second = cmds.listRelatives(f=True)
+            print("now crap_exists_second is: ", crap_exists_second)  # now crap_exists_second is:  ['|pl_chipOff_GRP|pCube2_pl_chipOff|pCube2_pl_sphere2|pCube2_pl_sphere2Shape']  # 复制出来一份
+            # 检查是否存在物体，并删除非网格类型的对象
+            if crap_exists_second and len(crap_exists_second) > 0:
+                for i in range(1, len(crap_exists_second)):
+                    if cmds.nodeType(crap_exists_second[i]) != "mesh":
+                        cmds.delete(crap_exists_second[i])
+            base = []
+            chunk = []
+            name = f"{firstTr[0]}_{rand}_pl_chunk1"
+            if check_grp is not None:
+                name = firstTr[0]
+            # 初始化临时列表
+            temp = []
+            if geo_hole == 1:
+                print("geo_hole == 0!!!")
+                # todo: 有洞的情况暂时先不考虑，让我们的脚本简单一点
+            else:
+                for d in range(firstCount):
+                    cmds.select(first[d], second[0])
+                    temp = cmds.duplicate()
+                    # 执行布尔操作
+                    if legacy > 0:
+                        cmds.polyBoolOp(temp, op=2, ch=0, uth=1, vdt=merge, n=name)
+                    else:
+                        # op=2:difference，cls：Changes how intersections of open and closed manifolds are treated. 1 for "Edge", 2 for "Normal".
+                        # ucb:If true, use the Carve Boolean library
+                        # uth:	If true, merge vertices with separations less then vertexDistanceThreshold and collapse faces with areas less then faceAreaThreshold. If false, do not merge vertices or collapse faces
+                        # vdt: Tolerance to determine whether vertices (and edges) should be merged before boolean operation is applied. Attribute is ignored unless useThresholds is set to true
+                        cmds.polyCBoolOp(temp, op=2, cls=1, ucb=1, ch=0, uth=1, vdt=merge, n=name) # n:Name of the new object
+                    # 中心化变换
+                    cmds.xform(cp=True)
+                    base.extend(cmds.listRelatives(f=True, c=True, type='mesh'))
+                    # cmds.delete(temp[0])
+                    # cmds.delete(temp[1])
+
+                    cmds.select(second[0])
+                    cmds.select(first[d], add=True)
+                    temp = cmds.duplicate()
+                    if legacy > 0:
+                        cmds.polyBoolOp(temp, op=3, ch=0, uth=1, vdt=merge, n=name)
+                    else:
+                        cmds.polyCBoolOp(temp, op=3, cls=1, ucb=1, ch=0, uth=1, vdt=merge, n=name)
+                    cmds.xform(cp=True)
+                    # 将新的网格添加到 chunk 列表中
+                    chunk.extend(cmds.listRelatives(f=True, c=True, type='mesh'))
+                    # 删除临时对象和第 d 个物体
+                    print("temp!!!", temp)  #
+                    # cmds.delete(temp)  # temp好像自动会被删除掉
+                    cmds.delete(first[d])
+            if cmds.objExists(second[0]):
+                cmds.delete(second[0])
+            print("grp!!!", grp)
+            if len(grp) > 0:
+                cmds.parent(base, grp[0])
+                base = cmds.ls(sl=True, l=True, tr=True)
+                cmds.parent(chunk, grp[0])
+                cmds.select(grp)
+            else:  # 暂时的逻辑应该都会进else
+                print("now we are in else!!!")
+                if not cmds.objExists(grpName):
+                    cmds.createNode('transform', name=grpName)
+
+                if not cmds.objExists(f"{firstTr[0]}_pl_shattered"):
+                    cmds.createNode('transform', name=f"{firstTr[0]}_pl_shattered", parent=grpName)
+
+                cmds.parent(base, f"|{grpName}|{firstTr[0]}_pl_shattered")
+                base = cmds.ls(sl=True, l=True, tr=True)
+                cmds.parent(chunk, f"|{grpName}|{firstTr[0]}_pl_shattered")
+            if firstCount > 1:
+                base = []
+
+            return base
 
     def pl_chipOffshatterBy(self):
-        print("pl_chipOffshatterBy!!!!")
+        org = cmds.ls(selection=True, long=True)
+        # 下面这一行将所有选中对象的子节点中类型为“网格”（mesh）的对象列表赋值给变量 orgShapeTemp。listRelatives 命令中的 children=True 查找子节点，fullPath=True 返回完整路径，type='mesh' 过滤结果，只包含网格对象。
+        orgShapeTemp = cmds.listRelatives(children=True, fullPath=True, type='mesh')
 
+        orgShape = cmds.ls(orgShapeTemp, long=True, noIntermediate=True)  # 非中间对象（Non-Intermediate Objects）指的是场景中实际用于渲染和显示的对象，而不是作为辅助或模板使用的对象
+        print("orgShape!!!", orgShape)
+        # orgShape : ['|pCube2|pCubeShape2', '|pl_chipOff_GRP|pCube2_pl_chipOff|pCube2_pl_sphere1|pCube2_pl_sphere1Shape', '|pl_chipOff_GRP|pCube2_pl_chipOff|pCube2_pl_sphere2|pCube2_pl_sphere2Shape', '|pl_chipOff_GRP|pCube2_pl_chipOff|pCube2_pl_sphere3|pCube2_pl_sphere3Shape', '|pl_chipOff_GRP|pCube2_pl_chipOff|pCube2_pl_sphere4|pCube2_pl_sphere4Shape']
+        result = cmds.checkBox('RESULT_ONLY', query=True, v=True)
+        delorg = cmds.checkBox('DEL', query=True, v=True)
+
+        cmds.pickWalk(direction='down')  # 这一行命令模拟鼠标操作，在对象层次结构中“向下选择”，有效地选择当前选择对象的下一级对象。
+        orgGrp = cmds.ls(selection=True, long=True, type='transform')
+        print("orgGrp!!!", orgGrp)  # orgGrp!!! []
+        cmds.pickWalk(direction='up')
+        if len(orgShape) > 2:  # 这种情况对应切四个顶点
+            geo = cmds.listRelatives(orgShape, parent=True, fullPath=True)
+            print("geo!!!" , geo) # ['|pCube2', '|pl_chipOff_GRP|pCube2_pl_chipOff|pCube2_pl_sphere1', '|pl_chipOff_GRP|pCube2_pl_chipOff|pCube2_pl_sphere2', '|pl_chipOff_GRP|pCube2_pl_chipOff|pCube2_pl_sphere3', '|pl_chipOff_GRP|pCube2_pl_chipOff|pCube2_pl_sphere4']
+            mainGeo = cmds.ls(geo, long=True, hd=1)  # hd: This flag specifies the maximum number of elements to be returned from the beginning of the list of items.
+            print("mainGeo!!!", mainGeo)  # mainGeo: ['|pCube2']
+            for i in range(1, len(geo)):
+                cmds.select(mainGeo, geo[i])  # mainGeo 是要被切的，geo[i] 是切mainGeo的对象
+                mainGeo = self.pl_shatterCutBy()
+        # todo：其他的情况还没有处理，我们先来写简单的情况
+
+        if len(orgShape) == 2:  # 这种情况对应选中四个顶点，但是但single mode，会把四个顶点作为一个整体切
+            self.pl_shatterCutBy()
 
     def setCutMode(self, preview, single_mode, use_plane, flat, result_only, legacy, min_scale, max_scale, min_div,
                      max_div):
